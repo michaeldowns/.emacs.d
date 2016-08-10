ef568a2bb9f511dc79aa0e15f4de01137a760df6
Michael Downs <msdowns@mitre.org>
2016-08-10 11:10:56 -0400

    More packages

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
   (setq package-list '(paradox neotree buffer-move magit web-mode jinja2-mode
 			       js2-mode flycheck json-mode auto-complete ac-js2
 			       js2-refactor elpy py-autopep8 expand-region
-			       multiple-cursors))
+			       multiple-cursors markdown-mode))
 
   (unless package-archive-contents
     (package-refresh-contents))
